777b24bebef769418d6e7d7d8f162986e8629d3b
mahesh-cse <maheshmyadav22@gmail.com>
2026-06-24 23:29:59 +0530

    Create userInput.java

---
+++ userInput.java
@@ -0,0 +1,9 @@
+import java,utill.scanner;
+class userInput{
+  public static void main(string[],args){
+   scanner sc = new scanner(sytem.in);
+    system.out.println("enter your name:");
+    string name = sc,nextline();
+    system.out.println("welcome"+ name);
+  }
+}
