708f179703ae828255fbf69a29c7ad7bfc972b44
mahesh-cse <maheshmyadav22@gmail.com>
2026-06-27 00:08:28 +0530

    Create VotingEligibility.java

---
+++ VotingEligibility.java
@@ -0,0 +1,9 @@
+class VotingEligibility {
+  public static void main(string[],args){
+    int age =18;
+    if(age >=18)
+      system.out.println("eligibe for voting");
+    else
+      system.out.println("not eligible for voting");
+  }
+}
