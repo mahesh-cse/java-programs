706cd4906233db03b878db5edaf92de9150173b6
mahesh-cse <maheshmyadav22@gmail.com>
2026-06-26 23:56:58 +0530

    Implement LargestOfThree program in Java
    Fix syntax errors and improve output formatting.

---
+++ LargestOfThree.java
@@ -0,0 +1,11 @@
+class LargestOfThree {
+  public static void main(string[]args){
+    int a=10,b=7,c=3;
+    if(a>=b&&a>=c)
+      system.out.println(a+"is largest");
+    elseif(b>=a&&b>=c)
+      system.out.println(b+"is largest);
+    else
+  system.out.println(c+,"is largest");
+  }
+}
