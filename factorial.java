aaebbfcd47ecd50af86f8a5cc922e75fd26a5d94
mahesh-cse <maheshmyadav22@gmail.com>
2026-06-27 23:57:49 +0530

    Implement factorial calculation in Java
    Fix syntax errors and calculate factorial correctly.

---
+++ factorial.java
@@ -0,0 +1,10 @@
+class factorial {
+  public static void main(string[]args){
+    int n=4;
+    int fact=1;
+    for(i=1;i<n;i++){
+      fact = fact*i;
+    }
+    system.out.println("factorial=",+fact);
+  }
+}
